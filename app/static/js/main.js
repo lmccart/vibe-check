37e46c29a3a38784358fbd0389c34fb2b6d29e5a
Lauren McCarthy <laurenleemccarthy@gmail.com>
2020-08-02 17:02:07 -0700

    vibe check ok

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,57 +1,87 @@
 let image_base = 'http://167.172.202.13/images/';
 let id = Number(window.location.pathname.substring(1));
+let leader_id = 1; // lead screen
 let expression;
 let photo_width, photo_height, photo_ratio;
 let screen_width = window.innerWidth;
 let mask_size = 3840;
 let mask_zoom = 10.0;
 let animation_duration = 5000;
-let pause_duration = 3000;
+let zoomed_in_pause_duration = 12000;
+let zoomed_out_pause_duration = 3000;
+let face_x, face_y, start_x;
+let blink_interval;
 
-$.get('/get_meta', data => {
-  console.log(data)
-  photo_width = data.photo_width;
-  photo_height = data.photo_height;
-  photo_ratio = window.innerHeight/photo_height;
-  expression = data.expressions[id];
+let getMeta = () => {
+  $.get('/get_meta', data => {
+    console.log(data)
+    photo_width = data.photo_width;
+    photo_height = data.photo_height;
+    photo_ratio = window.innerHeight/photo_height;
+    expression = data.expressions[id];
+  
+    $( '#label' ).css('color', data.colors[id]);
+    $( '#expression' ).css('color', data.colors[id]);
+    $( '#mask' ).attr('src', '/static/mask-'+expression+'.png');
+    
+    update(true);
+  });  
+}
+getMeta();
 
-  $( '#label' ).css('color', data.colors[id]);
-  $( '#expression' ).css('color', data.colors[id]);
-  $( '#mask' ).attr('src', '/static/mask-'+expression+'.png');
-
-  console.log(id, expression);
-  // setInterval(update, 3000);
-  update();
-});
-
-let update = () => {
+let update = (ready) => {
   $.get('/static/data.json', data => {
     console.log(data[expression]);
     let expression_data = data[expression];
     $('#photo').attr('src', image_base+expression_data.photo_path);
-    $('#expression').text(expression);
+    let label = expression === 'happiness' ? 'glee' : expression;
+    $('#expression').text(label);
 
-    let w = expression_data.rect[2] * photo_ratio;
-    let h = expression_data.rect[3] * photo_ratio;
-    let x = expression_data.rect[0] * photo_ratio;
-    let y = expression_data.rect[1] * photo_ratio;
+    let face_w = expression_data.rect[2] * photo_ratio;
+    let face_h = expression_data.rect[3] * photo_ratio;
+    face_x = expression_data.rect[0] * photo_ratio;
+    face_y = expression_data.rect[1] * photo_ratio;
     
     // check which side of photo to start from
-    let start_x = x < photo_width * photo_ratio * 0.5 ? -photo_width * photo_ratio + screen_width : 0;
-    reset(x, y, start_x);
-    zoomIn(x, y, start_x);
+    start_x = face_x < photo_width * photo_ratio * 0.5 ? -photo_width * photo_ratio + screen_width : 0;
+
+    if (ready) start();
   });
 };
 
+let start = () => {
+  reset();
+  let totalTime = zoomed_in_pause_duration + zoomed_out_pause_duration + 2 * animation_duration;
+  let millis = new Date().getTime();
+  let rem = millis % totalTime;
+  let diff = totalTime - rem;
+  if (diff < 1000) diff += totalTime;
+  setTimeout(function() {
+    zoomIn();
+    zoom_interval = setInterval(zoomIn, totalTime);
+  }, diff);
+  console.log(diff);
+}
+
 // reset photo and mask
-let reset = (x, y, start_x) => {
+let reset = () => {
+
+  blink(false);
+
   $( '#photo' ).css('height', '100%');
   $( '#photo' ).css('left', start_x);
   $( '#mask' ).css('width', mask_size * mask_zoom);
   $( '#mask' ).css('height', mask_size * mask_zoom);
-  $( '#mask' ).css('left', (x - mask_size * mask_zoom * 0.5));
-  $( '#mask' ).css('top', (y - mask_size * mask_zoom * 0.5));
+  $( '#mask' ).css('left', (face_x - mask_size * mask_zoom * 0.5));
+  $( '#mask' ).css('top', (face_y - mask_size * mask_zoom * 0.5));
+  $( '#label').hide();
+
   $( '#mask' ).show();
+  $( '#photo').stop();
+  $( '#mask').stop();
+  $( '#label').stop();
+  $( '#expression').stop();
+
 };
 
 // slide photo to final position
@@ -59,49 +89,63 @@ let slide = (x) => {
   let half = screen_width * 0.5;
   let target_x = half + (Math.random() - 0.5) * screen_width * 0.2;
   let left_diff = photo_width * photo_ratio - x;
-  console.log(photo_width * photo_ratio, x, left_diff, half)
   if (target_x > x) target_x = x;
   else if (left_diff < half) target_x = target_x + (half - left_diff);
   let offset = target_x - x;
-  $( '#photo' ).delay(pause_duration).animate({
+  $( '#photo' ).delay(zoomed_out_pause_duration).animate({
     left: offset
   }, animation_duration, 'swing');
   return target_x;
 }
 
 // zoom into face
-let zoomIn = (x, y, start_x) => {
-  $('#label').css('display', 'inline-block');
-  $('#label').addClass('blink');
+let zoomIn = () => {
+  blink(true);
 
-  let target_x = slide(x);
-  $( '#mask' ).delay(pause_duration).animate({
+  let target_x = slide(face_x);
+  $( '#mask' ).delay(zoomed_out_pause_duration).animate({
     width: mask_size,
     height: mask_size,
     left: target_x - mask_size * 0.5,
-    top: y - mask_size * 0.5,
-  }, animation_duration, 'swing', function () { zoomOut(x, y, start_x); });
+    top: face_y - mask_size * 0.5,
+  }, animation_duration, 'swing', zoomOut);
   
   setTimeout(function() { 
-    $('#label').removeClass('blink');
-    $('#expression').css('display', 'inline-block');
-  }, pause_duration + animation_duration);
+    blink(false);
+    $('#label').show();
+    $('#expression').css('margin-left', ((Math.random() > 0.5 ? 1 : -1) * 1.8) + 'em');
+  }, zoomed_out_pause_duration + animation_duration);
 }
 
 // zoom out of face
-let zoomOut = (x, y, start_x) => {
-  $( '#photo' ).delay(pause_duration).animate({
+let zoomOut = () => {
+  $( '#photo' ).delay(zoomed_in_pause_duration).animate({
     left: start_x
   }, animation_duration, 'swing');
-  $( '#mask' ).delay(pause_duration).animate({
+  $( '#mask' ).delay(zoomed_in_pause_duration).animate({
     width: mask_size * mask_zoom,
     height: mask_size * mask_zoom,
-    left: (x - mask_size * mask_zoom * 0.5),
-    top: (y - mask_size * mask_zoom * 0.5)
-  }, animation_duration, 'swing', function () { zoomIn(x, y, start_x); });
+    left: (face_x - mask_size * mask_zoom * 0.5),
+    top: (face_y - mask_size * mask_zoom * 0.5)
+  }, animation_duration, 'swing');
 
   setTimeout(function() { 
+    blink(true);
     $('#label').hide();
-    $('#expression').hide();
-  }, pause_duration);
+    $('#expression').css('margin-left', 0);
+  }, zoomed_in_pause_duration);
+
+  update();
 }
+
+let blink = (val) => {
+  if (blink_interval) clearInterval(blink_interval);
+  if (val) {
+    blink_interval = setInterval( function() { 
+      if ($( '#expression').css('display') === 'none') 
+        $( '#expression').show();
+      else
+        $( '#expression').hide();
+    }, 1000);
+  }
+};
