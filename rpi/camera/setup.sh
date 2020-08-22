639c987fcf7896db105baab33b971d87e8470ea2
Kyle McDonald <kyle@kylemcdonald.net>
2020-08-22 07:46:49 +0200

    rpi automation

--- rpi/camera/setup.sh
+++ rpi/camera/setup.sh
@@ -11,43 +11,6 @@
 # }
 # EOL
 
-# setup wpa to scan every 2 minutes
-# https://www.raspberrypi.org/forums/viewtopic.php?p=1432916#p1432916
-
-# cat >/usr/local/bin/wpaping <<EOL
-# #!/bin/bash
-# while : ;
-# do
-#     iwconfig wlan0 power off # disable power management
-#     wpa_cli -i wlan0 scan
-#     sleep 120
-# done
-# EOL
-
-# chmod +x /usr/local/bin/wpaping
-
-# cat >/lib/systemd/system/wpaping.service <<EOL
-# [Unit]
-# Description=WPA Supplicant pinger
-# Requires=network-online.target
-
-# [Service]
-# ExecStart=/usr/local/bin/wpaping
-# User=root
-# StandardInput=null
-# StandardOutput=null
-# StandardError=null
-# Restart=on-failure
-
-# [Install]
-# WantedBy=multi-user.target
-# EOL
-
-# systemctl daemon-reload
-
-# systemctl enable wpaping.service
-# systemctl start wpaping.service
-
 cat >/lib/systemd/system/vibecheck.service <<EOL
 [Unit]
 Description=Vibe Check Camera
