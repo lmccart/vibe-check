90a8eae52cd73c8810efdc783a0144f5bf30cc68
Kyle McDonald <kyle@kylemcdonald.net>
2020-08-25 16:22:06 -0700

    slightly less yellow

--- rpi/raw-to-jpg.py
+++ rpi/raw-to-jpg.py
@@ -5,10 +5,16 @@ import cv2
 from camera.RawProcessor import RawProcessor
 
 for fn in sys.argv[1:]:
-    out_fn = os.path.splitext(fn)[0] + '.jpg'
-    if os.path.exists(out_fn):
-        print('skipping', out_fn)
+    base, ext = os.path.splitext(fn)
+
+    if ext != '.raw':
         continue
+
+    out_fn = base + '.jpg'
+
+    # if os.path.exists(out_fn):
+    #     print('skipping', out_fn)
+    #     continue
 
     data = np.fromfile(fn, dtype=np.uint8)
 
@@ -16,8 +22,8 @@ for fn in sys.argv[1:]:
     curves = [
         [[0, 16], [127, 110], [255, 255]],
         [[0, 17], [127, 159], [255, 255]],
-        [[0, 17], [127, 137], [255, 255]]]
-    processor = RawProcessor(width, height, curves, mode='bgr')
+        [[0, 17], [127, 107], [255, 255]]]
+    processor = RawProcessor(width, height, curves[::-1], mode='bgr')
     img = processor(data)
 
     print('saving', out_fn)
