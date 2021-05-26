fa0487f2bb95c375effea8e607b1bb211e44147f
Kyle McDonald <kyle@kylemcdonald.net>
2021-05-26 20:27:19 +0200

    correct monitor order

--- automate/open-chrome.sh
+++ automate/open-chrome.sh
@@ -4,10 +4,10 @@ cd ~/Documents/vibe-check/automate
 
 killall chrome
 DISPLAY=:0.0 ./multibrowse \
+    http://localhost:8080/2 \
     http://localhost:8080/1 \
-    http://localhost:8080/0 \
-    http://localhost:8080/2
+    http://localhost:8080/0
 DISPLAY=:0.1 ./multibrowse \
-    http://localhost:8080/4 \
     http://localhost:8080/3 \
-    http://localhost:8080/5
+    http://localhost:8080/5 \
+    http://localhost:8080/4
